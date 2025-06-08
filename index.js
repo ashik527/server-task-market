fd6284ca388ec097b21c91d7082c875ccef57a2a
Ashik git project <ashikurrahmanshk@gmail.com>
2025-06-08 11:30:30 +0600

    modified for netlify

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://b11a10-task-market.netlify.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type'],
   credentials: true,
